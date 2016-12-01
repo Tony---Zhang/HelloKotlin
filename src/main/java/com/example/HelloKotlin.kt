317571d823f233001a252c54f59944620026a861
Zhang Shuai <520zhangshuai@gmail.com>
2016-12-01 22:37:27 +0800

    Deal will input price

--- src/main/java/com/example/HelloKotlin.kt
+++ src/main/java/com/example/HelloKotlin.kt
@@ -1,14 +1,17 @@
 package com.example
 
 import com.example.model.Item
+import java.util.*
 
 object Main {
 
     @JvmStatic
     fun main(args: Array<String>) {
-        println("Hello Kotlin")
-
+        val sc = Scanner(System.`in`)
+        println("Please input price: ")
+        val price = sc.nextFloat()
         val item = Item()
+        item.price = price
 
         println(item)
     }
