351b6080a2f0f33e3a58366340c421fef2acc25b
Zhang Shuai <520zhangshuai@gmail.com>
2016-12-01 22:35:43 +0800

    Add item data class

--- src/main/java/com/example/HelloKotlin.kt
+++ src/main/java/com/example/HelloKotlin.kt
@@ -1,9 +1,15 @@
 package com.example
 
+import com.example.model.Item
+
 object Main {
 
     @JvmStatic
     fun main(args: Array<String>) {
         println("Hello Kotlin")
+
+        val item = Item()
+
+        println(item)
     }
 }
---
+++ src/main/java/com/example/model/Item.kt
@@ -0,0 +1,5 @@
+package com.example.model
+
+data class Item(var price: Float = 0f) {
+
+}
